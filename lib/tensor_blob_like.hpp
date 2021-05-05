9c7dc6c7942475e3504b685d3e9b5ebb744f267b
gzr <guozhuoran@gmail.com>
2021-05-05 15:28:59 +0200

    update data_converter

--- lib/tensor_blob_like.hpp
+++ lib/tensor_blob_like.hpp
@@ -1,16 +1,22 @@
 
+#ifndef C_TENSOR_BLOB_LIKE_H
+#define C_TENSOR_BLOB_LIKE_H
+
+#include <boost/serialization/access.hpp>
+#include <caffe/data_transformer.hpp>
+#include "./util.hpp"
+
 namespace Ml {
 
     template<typename DType>
     class tensor_blob_like {
     public:
-        tensor_blob_like() : _empty(true) {};
+        tensor_blob_like() = default;
+
 
         void fromBlob(const caffe::Blob <DType> &blob) {
             //shape
             _shape = blob.shape();
-
-            _empty = blob.shape().empty();
 
             //data
             _data.resize(blob.count());
@@ -34,6 +40,44 @@ namespace Ml {
             std::memcpy(data_vec, _data.data(), _data.size() * sizeof(DType));
         }
 
+
+        void fromDatum(const caffe::Datum &datum){
+            //_empty = datum.has_data();
+            caffe::Blob<DType>* transformed_blob;
+            caffe::DataTransformer<DType> transformer;
+            transformer.Transform(datum,transformed_blob);
+            fromBlob(transformed_blob);
+
+        }
+        void toDatum(caffe::Datum &datum,bool reshape,bool label){
+            std::vector<int> datum_shape;
+            datum_shape[0] = (datum.has_data())?datum.data().size():0;
+            datum_shape[1] = (datum.has_channels()) ? datum.channels() : 0;
+            datum_shape[2] = (datum.has_height()) ? datum.height() : 0;
+            datum_shape[3] = (datum.has_width()) ? datum.width() : 0;
+
+            if (reshape){
+                datum.set_channels(_shape[1]);
+                datum.set_height(_shape[2]);
+                datum.set_width(_shape[3]);
+
+            }else{
+                if(datum_shape!=_shape){
+                    throw std::runtime_error("datum shape does not match");
+                }
+
+            }
+
+            if (label){
+                datum.set_label(_data.data(),_data.size());
+            }
+            else{
+                datum.set_data(_data.data(),_data.size());
+            }
+
+        }
+
+
         template<class Archive>
         void serialize(Archive &ar, const unsigned int version) {
             ar & _shape;
@@ -41,7 +85,7 @@ namespace Ml {
         }
 
         bool empty() {
-            return _empty;
+            return _data.empty();
         }
 
         GENERATE_GET(_shape, getShape
@@ -53,7 +97,8 @@ namespace Ml {
 
         std::vector<int> _shape;
         std::vector <DType> _data;
-        bool _empty;
     };
 
 }
+
+#endif
