e65da2181da05bde605f6ec471eb505f432608d4
twoentartian <twoentartian@hotmail.com>
2021-06-14 23:06:15 +0200

    checkpoint.

---
+++ lib/time_util.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+
+class time_util
+{
+public:
+	static time_t get_current_utc_time()
+	{
+		std::chrono::time_point currently = std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::system_clock::now());
+		std::chrono::duration seconds_since_utc_epoch = currently.time_since_epoch();
+		return seconds_since_utc_epoch.count();
+	}
+	
+	static std::string time_to_text(time_t utc_time)
+	{
+		std::time_t temp = utc_time;
+		std::tm* t = std::gmtime(&temp);
+		std::stringstream ss;
+		ss << std::put_time(t, "%Y-%m-%d %H:%M:%S %Z");
+		return ss.str();
+	}
+	
+	static time_t text_to_time(const std::string& text)
+	{
+		std::tm tmp_time;
+		strptime(text.c_str(),"%Y-%m-%d %H:%M:%S %Z",&tmp_time);
+		std::time_t t = timegm(&tmp_time);
+		return t;
+	}
+
+};
--- tests/miscellaneous_test/miscellaneous.cpp
+++ tests/miscellaneous_test/miscellaneous.cpp
@@ -1,29 +1,40 @@
-#include <ml_layer/fed_avg_buffer.hpp>
-
+#include <time_util.hpp>
 #include <byte_buffer.hpp>
+#include <time_util.hpp>
 
 #define BOOST_TEST_MAIN
+
 #include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE (miscellaneous_test)
-
-BOOST_AUTO_TEST_CASE (byte_buffer_test)
-{
-	uint64_t x = 473890;
-	uint32_t y = 372189;
-	std::string s = "asdwqe";
 	
-	byte_buffer buffers[2];
-	for (int i = 0; i < 2; ++i)
+	BOOST_AUTO_TEST_CASE (byte_buffer_test)
 	{
+		uint64_t x = 473890;
+		uint32_t y = 372189;
+		std::string s = "asdwqe";
+		
+		byte_buffer buffers[2];
+		for (int i = 0; i < 2; ++i)
+		{
 //		buffers[i].add(x);
 //		buffers[i].add(y);
 //		buffers[i].add(s);
-		buffers[i] << x << y << s;
+			buffers[i] << x << y << s;
+		}
+		BOOST_CHECK((buffers[0].size() == buffers[1].size()) && (buffers[0].size() == 18));
+		BOOST_CHECK(std::memcmp(buffers[0].data(), buffers[1].data(), buffers[0].size()) == 0);
 	}
-	BOOST_CHECK((buffers[0].size() == buffers[1].size()) && (buffers[0].size() == 18));
-	BOOST_CHECK(std::memcmp(buffers[0].data(), buffers[1].data(), buffers[0].size()) == 0);
-}
+	
+	BOOST_AUTO_TEST_CASE (time_converter_test)
+	{
+		auto now = time_util::get_current_utc_time();
+		auto now_str = time_util::time_to_text(now);
+		std::cout << "now str: " << now_str << std::endl;
+		auto now_copy = time_util::text_to_time(now_str);
+		BOOST_CHECK(now == now_copy);
+
+	}
 
 
-BOOST_AUTO_TEST_SUITE_END( )
+BOOST_AUTO_TEST_SUITE_END()
